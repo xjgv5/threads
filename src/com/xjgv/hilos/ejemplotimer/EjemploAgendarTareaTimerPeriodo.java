84c58a81143d52d4d8bb80e4f8842e20cdf65f2f
Javier Gomez <javier.gov@outlook.es>
2023-11-24 23:13:37 -0600

    Uso de Timer y TimerTast para agendar tareas

--- src/com/xjgv/hilos/ejemplotimer/EjemploAgendarTareaTimerPeriodo.java
+++ src/com/xjgv/hilos/ejemplotimer/EjemploAgendarTareaTimerPeriodo.java
@@ -1,20 +1,28 @@
 package com.xjgv.hilos.ejemplotimer;
 
+import java.awt.*;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EjemploAgendarTareaTimerPeriodo {
     public static void main(String[] args) {
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        AtomicInteger contadorAtomic = new AtomicInteger(3);
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
-            private int contador = 3;
+            //private int contador = 3;
             @Override
             public void run() {
-                if (contador > 0) {
-                    System.out.println("Tarea periodica en : " + new Date()
+                int i = contadorAtomic.getAndDecrement();
+                if (i > 0) {
+                    tk.beep();
+                    System.out.println("Tarea " + contadorAtomic + " periodica en : "
+                            + new Date()
                             + " - Nombre del Thread " + Thread.currentThread().getName());
-                    contador--;
+                    //contador--;
                 } else {
                     System.out.println("Finaliza el tiempo");
                     timer.cancel();
